a8abc994c6b1633e8b655c3629a9d18bcd599fa9
chenyong2github <yongchen1983.126.com@gmail.com>
2018-12-07 15:57:15 +0800

    rm

--- scripts/helper.cmake
+++
@@ -1,3 +0,0 @@
-set(GLSLANG_INSTALL_DIR "F:\VulkanSamples2\VulkanSamples\scripts\glslang/build/install" CACHE STRING "" FORCE)
-set(VULKAN_HEADERS_INSTALL_DIR "F:\VulkanSamples2\VulkanSamples\scripts\Vulkan-Headers/build/install" CACHE STRING "" FORCE)
-set(VULKAN_LOADER_INSTALL_DIR "F:\VulkanSamples2\VulkanSamples\scripts\Vulkan-Loader/build/install" CACHE STRING "" FORCE)
